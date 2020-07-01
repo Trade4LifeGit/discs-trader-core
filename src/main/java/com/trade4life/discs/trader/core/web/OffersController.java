14b8ed27868a9cb43aa883f80d1b9a14a7e11a7a
puzzleqw <staskozlovski506@gmail.com>
2020-07-01 08:26:50 +0300

    Added empty offer and statistics controllers for the next extension

---
+++ src/main/java/com/trade4life/discs/trader/core/web/OffersController.java
@@ -0,0 +1,15 @@
+package com.trade4life.discs.trader.core.web;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Api(value = "core-offers", tags = "core-offers")
+public class OffersController {
+}
