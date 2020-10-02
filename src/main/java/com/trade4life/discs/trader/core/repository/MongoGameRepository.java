c446294e3290b7707ce564b57bff6fcf87bc0896
puzzleqw <staskozlovski506@gmail.com>
2020-10-02 23:33:09 +0300

    updated mongo game repository

--- src/main/java/com/trade4life/discs/trader/core/repository/MongoGameRepository.java
+++ src/main/java/com/trade4life/discs/trader/core/repository/MongoGameRepository.java
@@ -1,8 +1,24 @@
 package com.trade4life.discs.trader.core.repository;
 
 import com.trade4life.discs.trader.core.service.dto.Game;
+import com.trade4life.discs.trader.core.service.dto.Title;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public interface MongoGameRepository extends MongoRepository<Game, String> {
-    //
+
+    @Query(value = "{'title': {$regex : ?0, $options: 'i'}}", fields = "{title : 1}")
+    List<Title> findTitlesByText(String titleText, Pageable pageable);
+
+    @Query(value = "{'title': {$regex : ?0, $options: 'i'}}")
+    List<Game> findGamesByTitlePart(String titlePart, Pageable pageable);
+
+    List<Game> findGamesByTitleIn(Set<String> fullTitles, Pageable pageable);
+
+    Optional<Game> findGameById(String id);
 }
