5cdc88d68b5427824ab912ecce28e6a6dd69f548
puzzleqw <staskozlovski506@gmail.com>
2020-10-09 09:48:27 +0300

    - deleted user mock - updated the user model

--- src/main/java/com/trade4life/discs/trader/core/service/dto/User.java
+++ src/main/java/com/trade4life/discs/trader/core/service/dto/User.java
@@ -1,5 +1,6 @@
 package com.trade4life.discs.trader.core.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -15,18 +16,21 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "users")
 @ApiModel(value = "User", description = "User information")
 public class User {
-    @ApiModelProperty(position = 1, example = "1")
-    private Integer id;
-    @ApiModelProperty(position = 2, example = "@puzzleqw")
+    @ApiModelProperty(position = 1, example = "5f7f6398c0418a5b8c52bbce")
+    @JsonIgnore
+    private String id;
+    @ApiModelProperty(position = 2, example = "dsvmwejfisvskmsa")
+    private String telegramId;
+    @ApiModelProperty(position = 3, example = "@puzzleqw")
     private String nickname;
-    @ApiModelProperty(position = 3, example = "Stanislau K.")
+    @ApiModelProperty(position = 4, example = "Stanislau K.")
     private String name;
-    @ApiModelProperty(position = 4, example = "+375296660818")
-    private String phone;
     @ApiModelProperty(position = 5, example = "+375296660818")
+    private String phone;
+    @ApiModelProperty(position = 6, example = "+375296660818")
     private String bio;
-    @ApiModelProperty(position = 6, allowableValues = "PSN, ESHOP")
+    @ApiModelProperty(position = 7, allowableValues = "PSN, ESHOP")
     private Platform platform;
-    @ApiModelProperty(position = 7)
+    @ApiModelProperty(position = 8)
     private Boolean isBlocked;
 }
