6342c2383ccc6eb7a95ba7118b1a147c1bd74779
puzzleqw <staskozlovski506@gmail.com>
2020-10-19 21:59:59 +0300

    - back to 0-based pageable - added totalPages & totalGames pagination fields

--- src/main/java/com/trade4life/discs/trader/core/service/impl/GamesServiceImpl.java
+++ src/main/java/com/trade4life/discs/trader/core/service/impl/GamesServiceImpl.java
@@ -6,6 +6,7 @@ import com.trade4life.discs.trader.core.service.dto.*;
 import com.trade4life.discs.trader.core.service.exception.CoreException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,16 +24,17 @@ public class GamesServiceImpl implements GamesService {
 
     @Override
     public TitleResponse findGameTitle(String titleText, Platform platform, Integer propositionSize) {
-        List<Title> propositions = StringUtils.isNotBlank(titleText) ?
+        Page<Title> propositionsPage = StringUtils.isNotBlank(titleText) ?
             gameRepository.findTitlesByText(titleText, PageRequest.of(0, propositionSize)) :
             gameRepository.findAllTitles(PageRequest.of(0, propositionSize));
 
-        boolean isOneLineSearchResult = propositionSize > 1 && propositions.size() == 1;
+        List<Title> pageContent = propositionsPage.getContent();
+        boolean isOneLineSearchResult = propositionSize > 1 && pageContent.size() == 1;
 
         return TitleResponse.builder()
             .platform(platform)
-            .title(isOneLineSearchResult ? propositions.get(0).getTitle() : null)
-            .propositions(propositions.stream()
+            .title(isOneLineSearchResult ? pageContent.get(0).getTitle() : null)
+            .propositions(pageContent.stream()
                 .map(Title::getTitle)
                 .collect(Collectors.toSet())
             )
@@ -41,14 +43,17 @@ public class GamesServiceImpl implements GamesService {
 
     @Override
     public GamesResponse findGamesByTitlePartAndPlatform(String titlePart, Platform platform, Pageable pageable) {
-        List<Game> games = StringUtils.isBlank(titlePart) ? gameRepository.findAllGames(pageable) :
+        Page<Game> gamesPage = StringUtils.isBlank(titlePart) ? gameRepository.findAllGames(pageable) :
             gameRepository.findGamesByTitlePart(titlePart, pageable);
 
+        List<Game> pageContent = gamesPage.getContent();
         return GamesResponse.builder()
             .platform(platform)
             .page(pageable.getPageNumber())
             .size(pageable.getPageSize())
-            .games(games)
+            .totalPages(gamesPage.getTotalPages())
+            .totalGames(gamesPage.getTotalElements())
+            .games(pageContent)
             .build();
     }
 
