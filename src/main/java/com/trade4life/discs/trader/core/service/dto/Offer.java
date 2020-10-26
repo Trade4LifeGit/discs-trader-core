3389172435d840fb110a34e412712dd224706db4
puzzleqw <staskozlovski506@gmail.com>
2020-10-26 19:04:53 +0300

    - added offer model dto's

--- src/main/java/com/trade4life/discs/trader/core/service/dto/Offer.java
+++ src/main/java/com/trade4life/discs/trader/core/service/dto/Offer.java
@@ -18,21 +18,19 @@ public class Offer {
     @ApiModelProperty(position = 1, example = "5f70948f17361f2260cb22a7")
     private String id;
     @ApiModelProperty(position = 2)
-    private Game game;
+    private String gameId;
     @ApiModelProperty(position = 3)
-    private User user;
+    private String userId;
     @ApiModelProperty(position = 4, example = "PSN")
     private Platform platform;
     @ApiModelProperty(position = 5, example = "BELARUS")
     private Region region;
     @ApiModelProperty(position = 6, example = "BUY")
     private OfferType type;
-    @ApiModelProperty(position = 7, example = "Open for swap")
+    @ApiModelProperty(position = 7, example = "Open for swap. Minsk. Nemiga")
     private String description;
     @ApiModelProperty(position = 8, example = "75")
     private Integer price;
-    @ApiModelProperty(position = 9, example = "Minsk. Nemiga")
-    private String location;
-    @ApiModelProperty(position = 10, example = "PENDING")
+    @ApiModelProperty(position = 9, example = "PENDING")
     private OfferStatus status;
 }
--- src/main/java/com/trade4life/discs/trader/core/service/dto/OfferSearchParams.java
+++ src/main/java/com/trade4life/discs/trader/core/service/dto/OfferSearchParams.java
@@ -1,5 +1,4 @@
 package com.trade4life.discs.trader.core.service.dto;
 
-public enum Region {
-    BELARUS, MOSCOW
+public class OfferSearchParams {
 }
