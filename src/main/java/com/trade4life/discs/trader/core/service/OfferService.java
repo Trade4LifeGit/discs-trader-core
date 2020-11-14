96cb6777ccef0d3ca122c7d618d5a143dde3dcae
puzzleqw <staskozlovski506@gmail.com>
2020-11-14 22:47:51 +0300

    - updated offer service

--- src/main/java/com/trade4life/discs/trader/core/service/OfferService.java
+++ src/main/java/com/trade4life/discs/trader/core/service/OfferService.java
@@ -1,35 +1,14 @@
 package com.trade4life.discs.trader.core.service;
 
 import com.trade4life.discs.trader.core.service.dto.*;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-
-import java.util.Optional;
 
 public interface OfferService {
-    //offers by user--- to check could i buy something else for one seller
-    Page<Offer> findOffersByUserId(String userId, Pageable pageable, Sort sort);
+    OfferResponse findOffersByStatus(OfferStatus offerStatus, Pageable pageable);
 
-    //offers by game
-    Page<Offer> findOffersByGameId(String gameId, Pageable pageable, Sort sort);
+    Offer findOfferById(String offerId);
 
-    //offers by region and platform
-    Page<Offer> findOffersByRegionAndPlatform(Region region, Platform platform, Pageable pageable, Sort sort);
+    Offer addNewOffer(Offer offer);
 
-    //offers by region
-    Page<Offer> findOffersByRegion(Region region, Pageable pageable, Sort sort);
-
-    //offers by type
-    Page<Offer> findOffersByType(OfferType offerType, Pageable pageable, Sort sort);
-
-    //offers by status
-    Page<Offer> findOffersByStatus(OfferStatus offerStatus, Pageable pageable, Sort sort);
-
-
-
-    //!!!!!!
-    Page<Offer> findOffersByParams(OfferSearchParams offerSearchParams, Pageable pageable);
-
-    Optional<Offer> findOffersById(String offerId);
+    Offer updateOffer(Offer offer);
 }
--- src/main/java/com/trade4life/discs/trader/core/service/impl/OfferServiceImpl.java
+++ src/main/java/com/trade4life/discs/trader/core/service/impl/OfferServiceImpl.java
@@ -1,51 +1,71 @@
 package com.trade4life.discs.trader.core.service.impl;
 
+import com.trade4life.discs.trader.core.repository.GameRepository;
+import com.trade4life.discs.trader.core.repository.OfferRepository;
 import com.trade4life.discs.trader.core.service.OfferService;
 import com.trade4life.discs.trader.core.service.dto.*;
+import com.trade4life.discs.trader.core.service.exception.CoreException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static com.trade4life.discs.trader.core.service.exception.CoreInternalErrorCode.OFFER_NOT_FOUND;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
 public class OfferServiceImpl implements OfferService {
+    private final OfferRepository offerRepository;
+    private final GameRepository gameRepository;
+
     @Override
-    public Page<Offer> findOffersByUserId(String userId, Pageable pageable, Sort sort) {
-        return null;
+    public OfferResponse findOffersByStatus(OfferStatus offerStatus, Pageable pageable) {
+
+        Page<Offer> publishedOffers = offerRepository.findOffersByStatus(OfferStatus.PUBLISHED, pageable);
+        Set<String> gameIds = publishedOffers.getContent().stream()
+            .map(Offer::getGameId)
+            .collect(Collectors.toSet());
+
+        Set<Game> games = gameRepository.findGamesByIdIn(gameIds);
+
+        Map<String, List<Offer>> offersByGameId = publishedOffers.getContent().stream()
+            .sorted(Comparator.comparing(Offer::getGameId).thenComparing(Offer::getPrice))
+            .collect(Collectors.groupingBy(Offer::getGameId));
+
+        return OfferResponse.builder()
+            .platform(Platform.PSN)
+            .page(pageable.getPageNumber())
+            .size(pageable.getPageSize())
+            .totalPages(publishedOffers.getTotalPages())
+            .totalOffers(publishedOffers.getTotalElements())
+            .offerGames(games)
+            .offersByGameId(offersByGameId)
+            .build();
     }
 
     @Override
-    public Page<Offer> findOffersByGameId(String gameId, Pageable pageable, Sort sort) {
-        return null;
+    public Offer findOfferById(String offerId) {
+        return offerRepository.findOfferById(offerId)
+            .orElseThrow(() -> new CoreException(OFFER_NOT_FOUND, NOT_FOUND));
     }
 
     @Override
-    public Page<Offer> findOffersByRegionAndPlatform(Region region, Platform platform, Pageable pageable, Sort sort) {
-        return null;
+    public Offer addNewOffer(Offer offer) {
+        if (offer.getId() != null) {
+            return updateOffer(offer);
+        }
+        offer.setStatus(OfferStatus.PUBLISHED);
+        return offerRepository.save(offer);
     }
 
     @Override
-    public Page<Offer> findOffersByRegion(Region region, Pageable pageable, Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<Offer> findOffersByType(OfferType offerType, Pageable pageable, Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<Offer> findOffersByStatus(OfferStatus offerStatus, Pageable pageable, Sort sort) {
-        return null;
-    }
-
-    @Override
-    public Page<Offer> findOffersByParams(OfferSearchParams offerSearchParams, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public Optional<Offer> findOffersById(String offerId) {
-        return Optional.empty();
+    public Offer updateOffer(Offer offer) {
+        offerRepository.findOfferById(offer.getId())
+            .orElseThrow(() -> new CoreException(OFFER_NOT_FOUND, NOT_FOUND));
+        return offerRepository.save(offer);
     }
 }
