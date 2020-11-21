bafa816e4f5767f79847bf676d7a0aa36d77671a
puzzleqw <staskozlovski506@gmail.com>
2020-11-21 23:47:44 +0300

    replaced titles with full game propositions

--- src/main/java/com/trade4life/discs/trader/core/service/GamesService.java
+++ src/main/java/com/trade4life/discs/trader/core/service/GamesService.java
@@ -6,9 +6,9 @@ import com.trade4life.discs.trader.core.service.dto.*;
 import org.springframework.data.domain.Pageable;
 
 public interface GamesService {
-    TitleResponse findGameTitle(String titlePart, Platform platform, Integer size);
+    GamePropositionResponse findGameByTitlePartAndPlatform(String titlePart, Platform platform, Integer size);
 
-    GameResponse findGamesByTitlePartAndPlatform(String titlePart, Platform platform, Pageable pageable);
+    GameResponse findAllGamesByTitlePartAndPlatform(String titlePart, Platform platform, Pageable pageable);
 
     Game findGameById(String id, Platform platform);
 }
