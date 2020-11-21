d6175cffae5bcdf1b98647fcc35f121afc9a7167
puzzleqw <staskozlovski506@gmail.com>
2020-11-21 23:45:30 +0300

    - title response dto was deleted - new game proposition response dto was added

--- src/main/java/com/trade4life/discs/trader/core/service/dto/GamePropositionResponse.java
+++ src/main/java/com/trade4life/discs/trader/core/service/dto/GamePropositionResponse.java
@@ -1,23 +1,24 @@
 package com.trade4life.discs.trader.core.service.dto;
 
+import com.trade4life.discs.trader.core.domain.Game;
 import com.trade4life.discs.trader.core.domain.Platform;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
-import java.util.Set;
+import java.util.List;
 
 @Builder
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel(value = "TitleResponse", description = "Title search result")
-public class TitleResponse {
+@ApiModel(value = "GamePropositionResponse", description = "Game search proposition")
+public class GamePropositionResponse {
     @ApiModelProperty(position = 1, allowableValues = "PSN, ESHOP")
     private Platform platform = Platform.PSN;
-    @ApiModelProperty(position = 2, example = "The Witcher 3: Wild Hunt")
-    private String title;
-    @ApiModelProperty(position = 3, example = "The Witcher 3: Wild Hunt — Hearts of Stone")
-    private Set<String> propositions;
+    @ApiModelProperty(position = 2)
+    private Game game;
+    @ApiModelProperty(position = 3)
+    private List<Game> propositions;
 }
