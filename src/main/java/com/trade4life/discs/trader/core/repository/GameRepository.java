53a9dbfb4db89c0da498a0bb7520344c904d3463
puzzleqw <staskozlovski506@gmail.com>
2020-11-21 23:46:38 +0300

    deleted extra title repository methods

--- src/main/java/com/trade4life/discs/trader/core/repository/GameRepository.java
+++ src/main/java/com/trade4life/discs/trader/core/repository/GameRepository.java
@@ -1,7 +1,6 @@
 package com.trade4life.discs.trader.core.repository;
 
 import com.trade4life.discs.trader.core.domain.Game;
-import com.trade4life.discs.trader.core.domain.Title;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,12 +10,6 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface GameRepository extends MongoRepository<Game, String> {
-
-    @Query(value = "{'title': {$regex : ?0, $options: 'i'}}", fields = "{title : 1}")
-    Page<Title> findTitlesByText(String titleText, Pageable pageable);
-
-    @Query(value = "{'title': {$ne : null}}", fields = "{title : 1}")
-    Page<Title> findAllTitles(Pageable pageable);
 
     @Query(value = "{'title': {$ne : null}}")
     Page<Game> findAllGames(Pageable pageable);
