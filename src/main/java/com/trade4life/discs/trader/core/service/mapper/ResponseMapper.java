7e5ce3fca7d440533cb52d03581d258b915966ab
puzzleqw <staskozlovski506@gmail.com>
2021-01-24 16:12:41 +0300

    feature/new-offer-games-response format : changed response mapper and related dto's

--- src/main/java/com/trade4life/discs/trader/core/service/mapper/ResponseMapper.java
+++ src/main/java/com/trade4life/discs/trader/core/service/mapper/ResponseMapper.java
@@ -1,16 +1,20 @@
 package com.trade4life.discs.trader.core.service.mapper;
 
 import com.trade4life.discs.trader.core.domain.Game;
+import com.trade4life.discs.trader.core.domain.Offer;
 import com.trade4life.discs.trader.core.domain.Platform;
 import com.trade4life.discs.trader.core.service.dto.GamePropositionResponse;
 import com.trade4life.discs.trader.core.service.dto.GameResponse;
+import com.trade4life.discs.trader.core.service.dto.GameWithRelatedOffers;
+import com.trade4life.discs.trader.core.service.dto.OfferGamesResponse;
 import org.mapstruct.Mapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@Mapper(componentModel="spring")
+@Mapper(componentModel = "spring")
 public interface ResponseMapper {
 
     default GameResponse toGameResponse(Page<Game> gamesPage, Platform platform, Pageable pageable) {
@@ -29,6 +33,31 @@ public interface ResponseMapper {
             .platform(platform)
             .game(game)
             .propositions(propositions)
+            .build();
+    }
+
+    default OfferGamesResponse toOfferGamesResponse(Set<Game> games, Page<Offer> publishedOffers, Platform platform, Pageable pageable) {
+
+        Map<String, List<Offer>> offersByGameId = publishedOffers.getContent().stream()
+            .sorted(Comparator.comparing(Offer::getGameId).thenComparing(Offer::getPrice))
+            .collect(Collectors.groupingBy(Offer::getGameId));
+
+        Set<GameWithRelatedOffers> offerGames = games.stream()
+            .map(game ->
+                GameWithRelatedOffers.builder()
+                    .game(game)
+                    .offers(offersByGameId.get(game.getId()))
+                    .build()
+            )
+            .collect(Collectors.toSet());
+
+        return OfferGamesResponse.builder()
+            .platform(Platform.PSN)
+            .page(pageable.getPageNumber())
+            .size(pageable.getPageSize())
+            .totalPages(publishedOffers.getTotalPages())
+            .totalOfferGames(publishedOffers.getTotalElements())
+            .offerGames(offerGames)
             .build();
     }
 }
